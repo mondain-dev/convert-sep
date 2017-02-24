a88f80ee16551658438def78f5336cbb3238474f
mondain-dev <mondain.sciamhaevensis@gmail.com>
2017-02-24 16:59:18 +0800

    Add files via upload

--- ConvertSEPHTML.py
+++ ConvertSEPHTML.py
@@ -2,6 +2,7 @@ import sys
 import re
 import requests
 import pypandoc
+import demjson
 from string import Template
 from bs4 import BeautifulSoup
 from urlparse import urljoin
@@ -24,15 +25,19 @@ def heading_HTMLEntity2TeX(html_entity):
   return heading_TeX
 
 def paragraph_HTMLEntity2TeX(html_entity):
-  paragraph_html = ''.join([unicode(e) for e in html_entity.contents])
-  return pypandoc.convert(paragraph_html, 'tex', format='html')
+  paragraph_TeX = pypandoc.convert(unicode(html_entity), 'tex', format='html+tex_math_single_backslash')
+  if re.search(r'\\textbackslash{}\(\\textbackslash\{\}ref\\{(.*)\\}\\textbackslash{}\)', paragraph_TeX):
+    paragraph_TeX = re.sub(r'\\textbackslash{}\(\\textbackslash\{\}ref\\{(.*)\\}\\textbackslash{}\)', r'\\ref{\1}', paragraph_TeX)
+  if re.search(r'\(\\textbackslash{}\((.*)\\textbackslash{}\)\)', paragraph_TeX):
+    paragraph_TeX = re.sub(r'\(\\textbackslash{}\((.*)\\textbackslash{}\)\)', (lambda x: ''.join(['($', re.sub(r'\\textbackslash{}', r'\\', x.group(1)), '$)'])), paragraph_TeX)
+  return paragraph_TeX
 
 def blockquote_HTMLEntity2TeX(html_entity):
   return ''.join(['\\blockquote{', paragraph_HTMLEntity2TeX(html_entity),'}'])
 
 def ul_HTMLEntity2TeX(html_entity):
   li_list = [''.join([unicode(e) for e in l.contents]) for l in html_entity.find_all('li')]
-  li_list_TeX = [ ''.join(['\\item ', pypandoc.convert(l, 'tex', format='html')]) for l in li_list ]
+  li_list_TeX = [ ''.join(['\\item ', pypandoc.convert(l, 'tex', format='html+tex_math_single_backslash')]) for l in li_list ]
   return ''.join(['\\begin{itemize}\n', ''.join(li_list_TeX), '\n\\end{itemize}'])
 
 def ol_HTMLEntity2TeX(html_entity):
@@ -43,14 +48,14 @@ def ol_HTMLEntity2TeX(html_entity):
   if html_entity.has_attr('type'):
     type = ''.join(['[', html_entity['type'],']'])
   li_list = [''.join([unicode(e) for e in l.contents]) for l in html_entity.find_all('li')]
-  li_list_TeX = [ ''.join(['\\item ', pypandoc.convert(l, 'tex', format='html')]) for l in li_list ]
+  li_list_TeX = [ ''.join(['\\item ', pypandoc.convert(l, 'tex', format='html+tex_math_single_backslash')]) for l in li_list ]
   return ''.join(['\\begin{enumerate}', type,'\n', counter, ''.join(li_list_TeX), '\n\\end{enumerate}'])
 
 def ConvertHTMLElement(html_element, sidenote=False):
   tag = html_element.name
   if tag == 'h2' or tag == 'h3' or tag == 'h4':
     return heading_HTMLEntity2TeX(html_element)
-  if tag == 'p':
+  if tag == 'p' or tag == 'div' or tag == 'span':
     return paragraph_HTMLEntity2TeX(html_element)
   if tag == 'blockquote':
     if sidenote:
@@ -69,14 +74,18 @@ def ConvertHTML(entry_html_entity):
       for i in entry_html_entity.children:
         if i.name:
           entry_TeX = '\n'.join([entry_TeX, ConvertHTMLElement(i)])
+        elif re.match(r'\\\[.*\\\]', str(i).strip(), re.DOTALL):
+          entry_TeX = '\n'.join([entry_TeX, str(i).strip()])
   return entry_TeX
 
-def OutputTeX(title, author, preamble='', main_text='', bibliography='', acknowledgments='', pubhistory='', copyright='', url=''):
+def OutputTeX(title, author, preamble='', main_text='', bibliography='', acknowledgments='', macros='', pubhistory='', copyright='', url=''):
   frontmatter_template=Template('\\documentclass[twoside]{tufte-book}\n\
 \\usepackage{soul}\n\
 \\usepackage{csquotes}\n\
 \\usepackage{graphicx}\n\
 \\usepackage{enumerate}\n\
+\\usepackage{amsmath}\n\
+\\usepackage{amssymb}\n\
 \n\
 \\usepackage{ifxetex}\n\
 \\ifxetex\n\
@@ -99,6 +108,8 @@ def OutputTeX(title, author, preamble='', main_text='', bibliography='', acknowl
 }\n\
 \\makeatother\n\
 \n\
+$macros\
+\n\
 \\title{$title}\n\
 \\author{$author}\n\
 \\begin{document}\n\
@@ -116,7 +127,7 @@ $copyright\n\
 \n\
 % \par The script used to generate this file can be found at \\url{https://github.com/mondain-dev/ConvertSEP/}\n\
 \\cleardoublepage\n')
-  string_TeX = frontmatter_template.substitute(title=title, author=author, copyright=copyright, pubhistory=pubhistory, url=url)
+  string_TeX = frontmatter_template.substitute(title=title, author=author, copyright=copyright, pubhistory=pubhistory, url=url, macros=macros)
   string_TeX = '\n'.join([string_TeX, preamble, '\n \\tableofcontents\n\n\\setcounter{secnumdepth}{2}',  main_text, bibliography, acknowledgments, '\\end{document}'])
   return string_TeX
 
@@ -133,6 +144,9 @@ def ProcessNotes(tex_src, base_url):
     note_num = None
     for i in soup.select('#aueditable')[0].children:
       if i.name:
+        if i.name == 'div':
+          if i.has_attr('id'):
+            note_num = i['id']
         for a in i.find_all('a'):
           if a.has_attr('name'):
             note_num = a['name']
@@ -177,7 +191,19 @@ def main():
   r  = requests.get(url)
   
   soup = BeautifulSoup(r.text)
-  entry_html = soup.select('#aueditable')[0]
+  
+  TeXMacros = ''
+  for s in soup.find_all('script'):
+    if s.has_attr('src'):
+      if s['src'] == 'local.js':
+        r_TeXMacros = requests.get( urljoin(url, s['src']) )
+        if re.search(r'window.MathJax\s*=\s*(.*);', r_TeXMacros.text, re.DOTALL):
+          js_snippet  = re.search(r'window.MathJax\s*=\s*(.*);', r_TeXMacros.text, re.DOTALL).group(1)
+          json_obj = demjson.decode(js_snippet)
+          if 'TeX' in json_obj:
+             if 'Macros' in json_obj['TeX']:
+               for m in json_obj['TeX']['Macros']:
+                 TeXMacros = '\n'.join([TeXMacros, ''.join(['\\newcommand{\\', m, '}{', json_obj['TeX']['Macros'][m], '}'])])
   
   title = soup.select('#aueditable h1')[0].text
   author_info = filter(None, [s.strip() for s in soup.select('#article-copyright')[0].text.split('by\n')[1].split('\n')])
@@ -206,7 +232,7 @@ def main():
           else:
             acknowledgments_TeX = '\n'.join([acknowledgments_TeX, ConvertHTMLElement(i)])
   
-  full_TeX   = OutputTeX(title, author, preamble_TeX, main_text_TeX, bibliography_TeX, acknowledgments_TeX, pubhistory_info, copyright_info, url)
+  full_TeX   = OutputTeX(title, author, preamble_TeX, main_text_TeX, bibliography_TeX, acknowledgments_TeX, TeXMacros, pubhistory_info, copyright_info, url)
   full_TeX   = ProcessNotes(full_TeX, url)
   output_file = open(output, "w")
   output_file.write(full_TeX.encode('utf8'))
